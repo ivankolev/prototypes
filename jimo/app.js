37860b1af91c6f8c984a578a01e8b5585ae2e0ed
Ivan Kolev <ivankolev@gmail.com>
2017-11-12 18:52:45 -0500

    experiments with express, passport and jwt

---
+++ jimo/app.js
@@ -0,0 +1,115 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var _ = require("lodash");
+var jwt = require('jsonwebtoken');
+
+var passport = require("passport");
+var passportJWT = require("passport-jwt");
+
+var ExtractJwt = passportJWT.ExtractJwt;
+var JwtStrategy = passportJWT.Strategy;
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var registeredUsers = [
+    {
+        id: 1,
+        name: 'jonathanmh',
+        password: '%2yx4'
+    },
+    {
+        id: 2,
+        name: 'test',
+        password: 'test'
+    }
+];
+var jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
+jwtOptions.secretOrKey = 'tasmanianDevil';
+
+var strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
+    console.log('payload received', jwt_payload);
+    // usually this would be a database call:
+    var user = registeredUsers[_.findIndex(registeredUsers, {id: jwt_payload.id})];
+    if (user) {
+        next(null, user);
+    } else {
+        next(null, false);
+    }
+});
+
+passport.use(strategy);
+
+var app = express();
+app.use(passport.initialize());
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+app.post("/login", function (req, res) {
+    if (req.body.name && req.body.password) {
+        var name = req.body.name;
+        var password = req.body.password;
+    }
+    // usually this would be a database call:
+    var user = users[_.findIndex(users, {name: name})];
+    if (!user) {
+        res.status(401).json({message: "no such user found"});
+    }
+
+    if (user.password === req.body.password) {
+        // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+        var payload = {id: user.id};
+        var token = jwt.sign(payload, jwtOptions.secretOrKey);
+        res.json({message: "ok", token: token});
+    } else {
+        res.status(401).json({message: "passwords did not match"});
+    }
+});
+
+app.get("/secret", passport.authenticate('jwt', {session: false}), function (req, res) {
+    res.json({message: "Success! You can not see this without a token"});
+});
+
+app.get("/secretDebug",
+    function (req, res, next) {
+        console.log(req.get('Authorization'));
+        next();
+    }, function (req, res) {
+        res.json("debugging");
+    });
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
